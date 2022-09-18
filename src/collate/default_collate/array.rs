c394364413b5d85c508394e739191d960be1ca23
Tudyx <56633664+Tudyx@users.noreply.github.com>
2022-09-18 21:42:29 +0200

    implementation for array

--- src/collate/default_collate/array.rs
+++ src/collate/default_collate/array.rs
@@ -1,43 +1,34 @@
-// use super::super::Collate;
-// use super::DefaultCollate;
+use super::super::Collate;
+use super::DefaultCollate;
 // use itertools::izip;
 // use ndarray::Array1;
 
-// impl<const N: usize> Collate<[i32; N]> for DefaultCollate {
-//     type Output = Vec<Array1<i32>>;
-//     fn collate(batch: Vec<[i32; N]>) -> Vec<Array1<i32>> {
-//         let mut it = batch.iter();
-//         let elem_size = it.next().unwrap().len();
-//         if !batch.iter().all(|vec| vec.len() == elem_size) {
-//             panic!("each element in list of batch should be of equal size");
-//         }
-//         let mut res = Vec::new();
-//         if batch.len() == 1 {
-//             res.push(DefaultCollate::collate(batch[0]));
-//         } else if batch.len() == 2 {
-//             for samples in izip!(batch[0], batch[1]) {
-//                 res.push(DefaultCollate::collate(samples))
-//             }
-//         } else if batch.len() == 3 {
-//             for samples in izip!(batch[0], batch[1], batch[2]) {
-//                 res.push(DefaultCollate::collate(samples));
-//             }
-//         }
-//         res
-//     }
-// }
+impl<T, const N: usize> Collate<[T; N]> for DefaultCollate
+where
+    DefaultCollate: Collate<T>,
+    T: Clone,
+{
+    type Output = Vec<<DefaultCollate as Collate<T>>::Output>;
+    fn collate(batch: Vec<[T; N]>) -> Self::Output {
+        let mut collated = Vec::with_capacity(batch.len());
+        for i in 0..batch[0].len() {
+            let vec: Vec<_> = batch.iter().map(|sample| sample[i].clone()).collect();
+            collated.push(DefaultCollate::collate(vec));
+        }
+        collated
+    }
+}
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use ndarray::array;
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ndarray::array;
 
-//     #[test]
-//     fn vec_of_array() {
-//         assert_eq!(
-//             DefaultCollate::collate(vec![[1, 2], [3, 4], [5, 6]]),
-//             vec![array![1, 3, 5], array![2, 4, 6]]
-//         );
-//     }
-
-// }
+    #[test]
+    fn vec_of_array() {
+        assert_eq!(
+            DefaultCollate::collate(vec![[1, 2], [3, 4], [5, 6]]),
+            vec![array![1, 3, 5], array![2, 4, 6]]
+        );
+    }
+}
